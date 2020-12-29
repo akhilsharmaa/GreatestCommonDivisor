b632634115f1def6429bd1e47035ceaad95bdf29
akhilsharmaa <akhilsharma.pro@gmail.com>
2020-12-29 16:45:59 +0530

    Initial commit

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,13 @@
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor (int first , int second ){
+        if ( first < 10 || second < 10 ){
+            return -1;
+        }
+
+        // Returns The greatest common divisor is 6 since both 12 and 30 can be divided by 6, and there is no resulting remainder.
+
+
+    }
+
+}
